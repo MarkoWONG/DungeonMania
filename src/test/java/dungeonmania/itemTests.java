35968fb9fec6e92315e563ad4d46ebb1261a5cf9
Marko <nfslolmiku@gmail.com>
2021-10-26 00:04:20 +1100

    added crafting and key tests using custom test maps

--- src/test/java/dungeonmania/itemTests.java
+++ src/test/java/dungeonmania/itemTests.java
@@ -1,11 +1,13 @@
 package dungeonmania;
 import dungeonmania.response.models.DungeonResponse;
+import dungeonmania.response.models.EntityResponse;
 import dungeonmania.response.models.ItemResponse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import org.junit.jupiter.api.Test;
 
 import dungeonmania.util.Direction;
+import dungeonmania.util.Position;
 
 public class itemTests {
     @Test
@@ -160,19 +162,138 @@ public class itemTests {
         assertFalse(new_frame.getInventory().contains(key2));
     }
 
-    // one key at a time (use a key (craft / open door) then pick up another key
-
-    // crafting
-
-    // using the potions
 
     // stat change when picked up a sword or armour
+
+    // using the potions
 
     // test one_ring item (respawn and drop rate)
 
     // durablity of bow and shield
 
+    // crafting
+    @Test
+    public void craftingOneAtATime(){
+        // Create new dungeon
+        DungeonManiaController dungeon = new DungeonManiaController();
+        DungeonResponse new_frame = dungeon.newGame("crafting", "standard");
+        ItemResponse bow = new ItemResponse("5", "bow");
+        ItemResponse shield1 = new ItemResponse("9", "shield");
+        ItemResponse shield2 = new ItemResponse("13", "shield");
+
+        for (int i = 0; i < 4; i++){
+            new_frame = dungeon.tick("none", Direction.RIGHT);
+        }
+        assertFalse(new_frame.getInventory().contains(bow));
+        new_frame = dungeon.build("bow");
+        assertTrue(new_frame.getInventory().contains(bow));
+
+        for (int i = 0; i < 3; i++){
+            new_frame = dungeon.tick("none", Direction.DOWN);
+        }
+        assertFalse(new_frame.getInventory().contains(shield1));
+        new_frame = dungeon.build("shield");
+        assertTrue(new_frame.getInventory().contains(shield1));
+
+        for (int i = 0; i < 3; i++){
+            new_frame = dungeon.tick("none", Direction.LEFT);
+        }
+        assertFalse(new_frame.getInventory().contains(shield2));
+        new_frame = dungeon.build("shield");
+        assertTrue(new_frame.getInventory().contains(shield2));
+    }
+    @Test
+    public void craftingAllAtATime(){
+        // Create new dungeon
+        DungeonManiaController dungeon = new DungeonManiaController();
+        DungeonResponse new_frame = dungeon.newGame("crafting", "standard");
+        ItemResponse bow = new ItemResponse("11", "bow");
+        ItemResponse shield1 = new ItemResponse("12", "shield");
+        ItemResponse shield2 = new ItemResponse("13", "shield");
+
+        for (int i = 0; i < 4; i++){
+            new_frame = dungeon.tick("none", Direction.RIGHT);
+        }
+        for (int i = 0; i < 3; i++){
+            new_frame = dungeon.tick("none", Direction.DOWN);
+        }
+        for (int i = 0; i < 3; i++){
+            new_frame = dungeon.tick("none", Direction.LEFT);
+        }
+        assertFalse(new_frame.getInventory().contains(bow));
+        new_frame = dungeon.build("bow");
+        assertTrue(new_frame.getInventory().contains(bow));
+        assertFalse(new_frame.getInventory().contains(shield1));
+        new_frame = dungeon.build("shield");
+        assertTrue(new_frame.getInventory().contains(shield1));
+        assertFalse(new_frame.getInventory().contains(shield2));
+        new_frame = dungeon.build("shield");
+        assertTrue(new_frame.getInventory().contains(shield2));
+    }
+    
+    // one key at a time (use a key (craft / open door) then pick up another key
+    @Test
+    public void usingKey(){
+        // Create new dungeon
+        DungeonManiaController dungeon = new DungeonManiaController();
+        DungeonResponse new_frame = dungeon.newGame("key_test", "standard");
+
+        new_frame = dungeon.tick("none", Direction.UP);
+        //find playerPosition
+        Position playerPos = findPlayerPos(new_frame);
+        // playerPosition should be in the same postion as player don't have a key
+        assertTrue(playerPos.equals(new Position(2,2)));
+
+        new_frame = dungeon.tick("none", Direction.RIGHT);
+        new_frame = dungeon.tick("none", Direction.UP);
+        playerPos = findPlayerPos(new_frame);
+        // playerPosition should be on top of the opened door (key used to open door)
+        assertTrue(playerPos.equals(new Position(3,1)));
+
+        new_frame = dungeon.tick("none", Direction.DOWN);
+        playerPos = findPlayerPos(new_frame);
+        assertTrue(playerPos.equals(new Position(3,2)));
+        
+        // playerPosition should be in the same postion as player don't have a key
+        new_frame = dungeon.tick("none", Direction.DOWN);
+        playerPos = findPlayerPos(new_frame);
+        assertTrue(playerPos.equals(new Position(3,2)));
+
+        // collect key and 2 wood and build a shield
+        new_frame = dungeon.tick("none", Direction.RIGHT);
+        new_frame = dungeon.tick("none", Direction.DOWN);
+        new_frame = dungeon.tick("none", Direction.DOWN);
+        new_frame = dungeon.build("shield");
+
+        // no key = locked door
+        new_frame = dungeon.tick("none", Direction.DOWN);
+        playerPos = findPlayerPos(new_frame);
+        assertTrue(playerPos.equals(new Position(4,4)));
+        
+        // collect key and open door
+        new_frame = dungeon.tick("none", Direction.RIGHT);
+        new_frame = dungeon.tick("none", Direction.DOWN);
+        playerPos = findPlayerPos(new_frame);
+        assertTrue(playerPos.equals(new Position(5,5)));
+    }
+
+    /**
+     * helper function to find player postion
+     * @param frame
+     * @return player postion or null if no player
+     */
+    public Position findPlayerPos(DungeonResponse frame){
+        for (EntityResponse ent : frame.getEntities()){
+            if (ent.getType().equals("player")){
+                return ent.getPosition();
+            }
+        }
+        return null;
+    }
+
     // test bomb
+
+    // one big intergation test
 
 
 
