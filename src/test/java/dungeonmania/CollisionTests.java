58dd9a427fd955f90121b73ffd44f0e031917c1e
Georgia Batson <g.batson@student.unsw.edu.au>
2021-10-27 01:00:29 +1100

    test stubs

---
+++ src/test/java/dungeonmania/CollisionTests.java
@@ -0,0 +1,42 @@
+package dungeonmania;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class CollisionTests {
+    @Test
+    public void testCollisions_playerToWall() {
+
+    }
+
+    @Test
+    public void testCollisions_spiderToWall() {
+
+    }
+
+    @Test
+    public void testCollisions_mobToWall() {
+
+    }
+
+    @Test
+    public void testCollisions_playerToDoor() {
+
+    }
+
+    @Test
+    public void testCollsisions_mobToDoor() {
+
+    }
+
+    @Test
+    public void testCollisions_playerToBoulder() {
+
+    }
+
+    @Test
+    public void testCollisions_mobToMob() {
+    }
+}
---
+++ src/test/java/dungeonmania/MovementTests.java
@@ -0,0 +1,33 @@
+package dungeonmania;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class MovementTests {
+    @Test
+    public void testMovement_player() {
+
+    }
+
+    @Test
+    public void testMovement_zombie() {
+
+    }
+
+    @Test
+    public void testMovement_mercenaryHostile() {
+
+    }
+
+    @Test
+    public void testMovement_spider() {
+
+    }
+
+    @Test
+    public void testMovement_mercenaryFriendly() {
+
+    }
+}
