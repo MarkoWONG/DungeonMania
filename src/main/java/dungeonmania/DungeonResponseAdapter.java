6530b62e341de56bc190619d37a6335be7fbd3fe
Ethan Dickson <ethanndickson@gmail.com>
2021-10-28 00:03:07 +1100

    adapter stubs

--- src/main/java/dungeonmania/DungeonResponseAdapter.java
+++ src/main/java/dungeonmania/DungeonResponseAdapter.java
@@ -1,5 +1,13 @@
 package dungeonmania;
 
-// Adapter
+import dungeonmania.response.models.DungeonResponse;
+
+// Very loosely follows the Adapter Pattern
 public class DungeonResponseAdapter {
+
+    private Dungeon dungRef;
+
+    static DungeonResponse createDungResponse(Dungeon dungeon) {
+        return new DungeonResponse();
+    }
 }
