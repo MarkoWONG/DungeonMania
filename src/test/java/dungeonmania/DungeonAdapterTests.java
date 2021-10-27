b71b900ecf9a4acc673e5e6923d6604e9ea58bb0
Ethan Dickson <ethanndickson@gmail.com>
2021-10-28 00:00:21 +1100

    dungeon adapter tests done

--- src/test/java/dungeonmania/DungeonAdapterTests.java
+++ src/test/java/dungeonmania/DungeonAdapterTests.java
@@ -1,2 +1,75 @@
-package dungeonmania;public class DungeonAdapterTests {
+package dungeonmania;
+
+import dungeonmania.difficulty.Standard;
+import dungeonmania.entity.Entity;
+import dungeonmania.response.models.DungeonResponse;
+import dungeonmania.response.models.EntityResponse;
+import dungeonmania.response.models.ItemResponse;
+import dungeonmania.util.Direction;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DungeonAdapterTests {
+
+    @Test
+    public void testDungeonName() {
+        Dungeon currDungeon = new Dungeon("dungeon_static","Standard");
+        DungeonResponse currResponse = DungeonResponseAdapter.createDungResponse(currDungeon);
+        assertEquals("dungeon_static",currResponse.getDungeonName());
+    }
+
+    @Test
+    public void testEntityOnly() {
+        Dungeon currDungeon = new Dungeon("dungeon_static","Standard");
+        DungeonResponse currResponse = DungeonResponseAdapter.createDungResponse(currDungeon);
+        assertEquals(7, (int) currResponse.getEntities().size());
+        // this test is already done by DungeonTests (more or less), so we'll keep it short
+    }
+
+    @Test
+    public void testItemResponse() {
+        Dungeon currDungeon = new Dungeon("dungeon_collectable","Standard");
+        IntStream.range(0,10).forEach(tick -> currDungeon.tick(null, Direction.DOWN));
+        DungeonResponse currResponse = DungeonResponseAdapter.createDungResponse(currDungeon);
+        List<String> allItems = currResponse.getInventory().stream().map(ItemResponse::getType).collect(Collectors.toList());
+        assertEquals(10, (int) allItems.size());
+        assertTrue(allItems.contains("key"));
+        assertTrue(allItems.contains("health_potion"));
+        assertTrue(allItems.contains("invincibility_potion"));
+        assertTrue(allItems.contains("invisibility_potion"));
+        assertTrue(allItems.contains("wood"));
+        assertTrue(allItems.contains("arrow"));
+        assertTrue(allItems.contains("bomb"));
+        assertTrue(allItems.contains("sword"));
+        assertTrue(allItems.contains("armour"));
+        assertTrue(allItems.contains("one_ring"));
+    }
+
+    @Test
+    public void testBuildableContents() {
+        Dungeon currDungeon = new Dungeon("buildableEntities","Standard");
+        IntStream.range(0,6).forEach(tick -> currDungeon.tick(null, Direction.UP));
+        DungeonResponse currResponse = DungeonResponseAdapter.createDungResponse(currDungeon);
+        List<String> allBuildables = currResponse.getBuildables();
+        assertEquals(2, allBuildables.size());
+        assertTrue(allBuildables.contains("bow"));
+        assertTrue(allBuildables.contains("shield"));
+    }
+
+    @Test
+    public void testGoalContents() {
+        Dungeon currDungeon = new Dungeon("advanced","Standard");
+        DungeonResponse currResponse = DungeonResponseAdapter.createDungResponse(currDungeon);
+        String goals = currResponse.getGoals();
+        assertTrue(goals.contains("enemies"));
+        assertTrue(goals.contains("treasure"));
+        assertTrue(goals.contains("AND"));
+    }
+
 }
