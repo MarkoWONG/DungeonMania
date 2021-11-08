22b995b992a7b1fddb169c57a2c335d75a083847
Georgia Batson <g.batson@student.unsw.edu.au>
2021-11-09 00:34:14 +1100

    dijkstra algo, untested

--- src/main/java/dungeonmania/movement/MovementManager.java
+++ src/main/java/dungeonmania/movement/MovementManager.java
@@ -10,6 +10,11 @@ import dungeonmania.util.Direction;
 import dungeonmania.util.Position;
 import java.lang.Math;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
 import java.util.Random;
 
 
@@ -159,6 +164,28 @@ public class MovementManager {
         return true;
     }
 
+    public static Boolean staticCheckMove(Entity entity, Position position, EntityList entities) {
+        Position newEntityPosition = position;
+        ArrayList<Entity> tile = entities.search(newEntityPosition);
+        for (Entity eachEntity : tile) {
+            if (eachEntity instanceof Door && entity instanceof PlayerCharacter) {
+                return (((Door) eachEntity).unlockDoor((PlayerCharacter) entity));
+            }
+            if (eachEntity.getPosition().getLayer() >= entity.getPosition().getLayer()) {
+                if ((eachEntity instanceof Mob || eachEntity instanceof PlayerCharacter) && (entity instanceof Mob || entity instanceof PlayerCharacter)) {
+                    return true;
+                }
+                if ((eachEntity.getType().equals("spider") && entity.getType().equals("boulder"))) {
+                    ((Spider) eachEntity).changeDirection();
+                } else if ((eachEntity.getType().equals("boulder") && entity.getType().equals("spider"))) {
+                    ((Spider) entity).changeDirection();
+                }
+                return false;
+            }
+        }
+        return true;
+    }
+
     /**
      * 
      * @param a some entity a
@@ -166,7 +193,7 @@ public class MovementManager {
      * @return the direction a must travel to get to b
      */
     public static Direction shortestPath(Entity a, Position b, EntityList entities) {
-        Position btwn = Position.calculatePositionBetween(a.getPosition(), b);
+        /*Position btwn = Position.calculatePositionBetween(a.getPosition(), b);
         int xDistance = btwn.getX();
         int yDistance = btwn.getY();
         Direction d = Direction.NONE;
@@ -182,6 +209,8 @@ public class MovementManager {
         } else {
             return Direction.NONE;
         }
+        */
+        return dijksra(a, b, entities);
     }
 
     public static Direction invertDirection(Direction d) {
@@ -200,6 +229,78 @@ public class MovementManager {
         else{
             return Direction.NONE;
         }
+    }
+
+    private static Direction dijksra(Entity a, Position b, EntityList entities) {
+        List<Position> grid = entities.grid(a);
+
+        //let dist be a Map<Position, Double>
+        Map<Position, Double> dist = new HashMap<>();
+        //let prev be a Map<Position, Position>
+        Map<Position, Position> prev = new HashMap<>();
+        //let queue be a Queue<Position> of every position in grid
+        List<Position> queue = new ArrayList<>();
+
+        //for each Position p in grid:
+        //    dist[p] := infinity
+        //    previous[p] := null
+        //dist[source] := 0
+        for (Position p : grid) {
+            dist.put(p, Double.POSITIVE_INFINITY);
+            prev.put(p, null);
+            queue.add(p);
+        }
+        dist.replace(a.getPosition(), 0.0);
+
+        while (!queue.isEmpty()) {
+            // get node with smallest distance
+            Position u = getDijkstraMin(queue, dist);
+            queue.remove(u);
+
+            // get neighbours
+            List<Position> neighbours = u.getAdjacentPositions();
+
+            for (Position v : neighbours) {
+                if (queue.contains(v)) {
+                    Double tempDistance = dist.get(u) + 1; // * u.movementFactor();
+                    if (tempDistance < dist.get(v)) {
+                        dist.replace(v, tempDistance);
+                        prev.replace(v, u);
+                    }
+                }
+            }
+        }
+
+        // return the direction of the shortest path
+
+        Direction path = Direction.NONE;
+        Double min = Double.POSITIVE_INFINITY;
+
+        List<Position> neighbours = a.getPosition().getAdjacentPositions();
+        for (int i = 1; i < 8; i = i+2) { // we only want directly adjacent, not diagonal
+            if (dist.get(neighbours.get(i)) < min) {
+                min = dist.get(neighbours.get(i));
+                if (i == 1) {path = Direction.UP;}
+                if (i == 3) {path = Direction.RIGHT;}
+                if (i == 5) {path = Direction.DOWN;}
+                if (i == 7) {path = Direction.LEFT;}
+                
+            }
+        }
+
+        return path;
+    }
+
+    private static Position getDijkstraMin(List<Position> queue, Map<Position, Double> dist) {
+        Position shortest = null;
+        Double min = Double.POSITIVE_INFINITY; 
+        for (Position p : queue) {
+            if (dist.get(p) < min) {
+                shortest = p;
+                min = dist.get(p);
+            }
+        }
+        return shortest;
     }
 
 }
