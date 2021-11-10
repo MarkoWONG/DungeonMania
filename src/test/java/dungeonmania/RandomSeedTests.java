454cbdd2fc10ba733a905917415f1ae47cc301f4
Ethan Dickson <ethanndickson@gmail.com>
2021-11-11 10:19:38 +1100

    fixed random seed test

--- src/test/java/dungeonmania/RandomSeedTests.java
+++ src/test/java/dungeonmania/RandomSeedTests.java
@@ -4,9 +4,12 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import dungeonmania.response.models.DungeonResponse;
+import dungeonmania.response.models.EntityResponse;
 import dungeonmania.util.Direction;
 import dungeonmania.util.Position;
 import org.junit.jupiter.api.Test;
+
+import java.util.stream.Collectors;
 
 public class RandomSeedTests {
 
@@ -31,20 +34,29 @@ public class RandomSeedTests {
         response2 = controller2.tick(null, Direction.DOWN);
         response3 = controller3.tick(null, Direction.DOWN);
 
+        assertEquals(response1.getEntities().stream().map(EntityResponse::getPosition).collect(Collectors.toList()), response2.getEntities().stream().map(EntityResponse::getPosition).collect(Collectors.toList()));
+        assertEquals(response2.getEntities().stream().map(EntityResponse::getPosition).collect(Collectors.toList()), response3.getEntities().stream().map(EntityResponse::getPosition).collect(Collectors.toList()));
 
         response1 = controller1.tick(null, Direction.LEFT);
         response2 = controller2.tick(null, Direction.LEFT);
         response3 = controller3.tick(null, Direction.LEFT);
 
+        assertEquals(response1.getEntities().stream().map(EntityResponse::getPosition).collect(Collectors.toList()), response2.getEntities().stream().map(EntityResponse::getPosition).collect(Collectors.toList()));
+        assertEquals(response2.getEntities().stream().map(EntityResponse::getPosition).collect(Collectors.toList()), response3.getEntities().stream().map(EntityResponse::getPosition).collect(Collectors.toList()));
 
         response1 = controller1.tick(null, Direction.UP);
         response2 = controller2.tick(null, Direction.UP);
         response3 = controller3.tick(null, Direction.UP);
 
+        assertEquals(response1.getEntities().stream().map(EntityResponse::getPosition).collect(Collectors.toList()), response2.getEntities().stream().map(EntityResponse::getPosition).collect(Collectors.toList()));
+        assertEquals(response2.getEntities().stream().map(EntityResponse::getPosition).collect(Collectors.toList()), response3.getEntities().stream().map(EntityResponse::getPosition).collect(Collectors.toList()));
 
         response1 = controller1.tick(null, Direction.RIGHT);
         response2 = controller2.tick(null, Direction.RIGHT);
         response3 = controller3.tick(null, Direction.RIGHT);
+
+        assertEquals(response1.getEntities().stream().map(EntityResponse::getPosition).collect(Collectors.toList()), response2.getEntities().stream().map(EntityResponse::getPosition).collect(Collectors.toList()));
+        assertEquals(response2.getEntities().stream().map(EntityResponse::getPosition).collect(Collectors.toList()), response3.getEntities().stream().map(EntityResponse::getPosition).collect(Collectors.toList()));
 
     }
 }
